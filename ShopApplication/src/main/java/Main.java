29d44595c36bca8115afe30a1e58ae1ef8044736
Shayan Mohammadi <shayan.mkk@gmail.com>
2020-05-15 11:07:06 +0430

    load database and set superCategory

--- ShopApplication/src/main/java/Main.java
+++ ShopApplication/src/main/java/Main.java
@@ -1,8 +1,16 @@
-import controller.*;
-        import view.View;
+import controller.AdminController;
+import controller.Controller;
+import controller.CustomerController;
+import controller.SellerController;
+import model.Category;
+import model.database.DatabaseManager;
+import view.View;
 
 public class Main {
     public static void main(String[] args) {
+        new DatabaseManager().loadDatabase();
+        Category.setSuperCategory();
+
         View appView = new View(new Controller(), new SellerController(), new AdminController(), new CustomerController());
         appView.start();
     }
