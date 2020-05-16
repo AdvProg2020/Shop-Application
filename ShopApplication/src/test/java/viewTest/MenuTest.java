c7192ef1ec19e1efe86918f14a47e834cd10a0ad
Shayan Mohammadi <shayan.mkk@gmail.com>
2020-05-16 19:43:11 +0430

    starting unit tests

---
+++ ShopApplication/src/test/java/viewTest/MenuTest.java
@@ -0,0 +1,66 @@
+package viewTest;
+
+import org.junit.jupiter.api.Test;
+
+class MenuTest {
+
+    @Test
+    void initSubMenus() {
+    }
+
+    @Test
+    void initSubActions() {
+    }
+
+    @Test
+    void setAccountMenu() {
+    }
+
+    @Test
+    void setProductDetailMenu() {
+    }
+
+    @Test
+    void getAccountMenu() {
+    }
+
+    @Test
+    void getProductDetailMenu() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getCommandPattern() {
+    }
+
+    @Test
+    void showCommandList() {
+    }
+
+    @Test
+    void printArray() {
+    }
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void show() {
+    }
+
+    @Test
+    void execute() {
+    }
+
+    @Test
+    void getCallingMenu() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
