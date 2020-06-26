4061395bf992dba3db45dab4333b89bd68dc2bc5
ShayanTalaei <talaee.shayan@gmail.com>
2020-06-26 22:29:12 +0430

    product box comparing passing product id in methods

--- ShopApplication/src/main/java/view/Controllers.java
+++ ShopApplication/src/main/java/view/Controllers.java
@@ -1180,11 +1180,11 @@ public class Controllers {
         public void initialize(URL url, ResourceBundle resourceBundle) {
 
             for (String[] subProductPack : mainController.getSubProductsForAdvertisements(6)) {
-                advertisingProducts.getChildren().add(ProductBoxController.createBox(subProductPack, true));
+                advertisingProducts.getChildren().add(ProductBoxController.createBox(subProductPack, null));
             }
 
             for (String[] subProduct : mainController.getSubProductsInSale(10)) {
-                productsInSale.getChildren().add(ProductBoxController.createBox(subProduct, true));
+                productsInSale.getChildren().add(ProductBoxController.createBox(subProduct, null));
             }
 
             allSales.setOnAction(e -> salesMenu());
@@ -1376,7 +1376,7 @@ public class Controllers {
             scrollPane.setContent(productsPane);
             int index = 0;
             for (String[] subProductPack : products) {
-                Parent productBox = ProductBoxController.createBox(subProductPack, !toCompare);
+                Parent productBox = ProductBoxController.createBox(subProductPack, productIdToCompareWith);
                 productsPane.add(productBox, index % numberOfColumns, index / numberOfColumns);
                 index++;
             }
@@ -1493,14 +1493,14 @@ public class Controllers {
         private String[] subProduct;
 
 
-        public static Parent createBox(String[] subProduct, boolean show) {
+        public static Parent createBox(String[] subProduct, String productToCompare) {
             FXMLLoader loader = new FXMLLoader(View.class.getResource("/fxml/" + Constants.FXMLs.productBox + ".fxml"));
             Parent p;
             try {
                 p = loader.load();
                 ProductBoxController pbc = loader.getController();
                 pbc.setInfo(subProduct);
-                pbc.setAction(p, show);
+                pbc.setAction(p, productToCompare);
                 return p;
             } catch (IOException e) {
                 e.printStackTrace();
@@ -1540,9 +1540,10 @@ public class Controllers {
         }
 
         //TODO: else: compare
-        private void setAction(Parent p, boolean show) {
-            if(show)
+        private void setAction(Parent p, String productToCompare) {
+            if(productToCompare == null)
                 p.setOnMouseClicked(e -> ProductDetailMenuController.display(subProduct[0], subProduct[1], false));
+
         }
 
         private void initRatingStars(double rating) {
